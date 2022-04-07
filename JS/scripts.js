ba33c0b51b1b93bba63008e4875bf443160ed9a0
Louis Knolle <knollelw@gmail.com>
2022-04-07 14:55:31 -0400

    add margin to forms and define form responses as const in scripts.js

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
-  
+  $('form#formOne').submit(function(event) {
+    event.preventDefault();
+    const whichStack = $('#whichStack').val();
+    const experience = $('#experience').val();
+    const cliffJump = $('#cliffJump').val();
+    const age = parseInt($('#age').val());
+    const dogCat = $('#dogCat').val();
+
+    
+  });
 });
