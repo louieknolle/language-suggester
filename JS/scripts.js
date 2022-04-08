06c7eee8bd842ba09dd21f98d1c7478291b7c51e
Louis Knolle <knollelw@gmail.com>
2022-04-07 20:37:49 -0400

    add img files for language results and begin branching logic

--- JS/scripts.js
+++ JS/scripts.js
@@ -7,6 +7,14 @@ $(document).ready(function() {
     const age = parseInt($('#age').val());
     const dogCat = $('#dogCat').val();
 
-    
+    if (whichStack === 'Frontend developer' || experience === 'No experience') {
+      $('#javaScript').toggle();
+    } else if (whichStack === 'Backend developer' || experience === 'Some experience') {
+      $('#ruby').toggle();
+    } else if (whichStack === 'Full stack' || experience === 'Lots of experience') {
+      $('#cSharp').toggle();
+    } else if (cliffJump === 'Yes') {
+      $('')
+    }
   });
 });
